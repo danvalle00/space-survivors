3b18f1bd6529e9b0937ad87c9cc2a5866ea89c6e
danvalle00 <danieldovalle10@gmail.com>
2025-10-28 17:01:17 -0300

    setup gitignore and player 8d movement

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,16 +1,32 @@
-using UnityEngine;
 
+using UnityEngine;
+using UnityEngine.InputSystem;
+[RequireComponent(typeof(Rigidbody2D))]
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public InputSystem_Actions inputSystem;
+    private Rigidbody2D playerRb;
+
+    [SerializeField] private float moveSpeed = 1f;
+    void Awake()
     {
-        
+        inputSystem = new InputSystem_Actions();
+        playerRb = GetComponent<Rigidbody2D>();
+    }
+    void OnEnable()
+    {
+        inputSystem.Player.Enable();
+    }
+    void OnDisable()
+    {
+        inputSystem.Player.Disable();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Vector2 movementInput = inputSystem.Player.Movement.ReadValue<Vector2>();
+        playerRb.linearVelocity = moveSpeed * movementInput;
     }
+
 }
+
